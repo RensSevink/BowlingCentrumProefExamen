62262afa69504ee14481157200c85adf4a708263
Gustas2077 <333160@student.mboutrecht.nl>
2023-03-28 10:27:35 +0200

    Migration&Model

--- app/Models/OccasionOptions.php
+++ app/Models/OccasionOptions.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class OccasionOptions extends Model
 {
     use HasFactory;
+
+    // fillable array is nodig om de velden te kunnen vullen
+    protected $fillable = [
+        'OrderId',
+        'TypeOfOccasion',
+        'IsActive',
+        'Note',
+    ];
+    
+    // function om de relatie met de tabel orders te maken
+    public function order()
+    {
+        return $this->belongsTo(Order::class, 'OrderId');
+    }
 }
--- database/migrations/2023_03_28_073127_create_occasion_options_table.php
+++ database/migrations/2023_03_28_073127_create_occasion_options_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('occasion_options', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('OrderId')->constrained('order')->onDelete('cascade');
+            $table->varchar('TypeOfOccasion');
+            $table->boolean('IsActive');
+            $table->varchar('Note');
+            $table->date('DateCreated');
+            $table->date('DateChanged');
         });
     }
 
