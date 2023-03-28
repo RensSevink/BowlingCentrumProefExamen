c455fa88c21f2f0f3dab6895ab70d5abb72177c7
Gustas2077 <333160@student.mboutrecht.nl>
2023-03-28 11:28:24 +0200

    commit 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Reservation</title>
+</head>
+<body>
+<H1>Reservation</H1>
+<img src="https://media.newyorker.com/photos/59095bb86552fa0be682d9d0/master/w_2560%2Cc_limit/Monkey-Selfie.jpg" alt="" width="500" height="600">
+</body>
+</html>
