9589aa03a9ee15455aea783b2350905d58a20a91
RensSevink <332726@student.mboutrecht.nl>
2023-03-28 11:25:18 +0200

    read and delete medewerker added
    read en delete is nu toegevoegd op de view pagina van de medewerker

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('contact.index', [
+            'contact' => Contact::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('contact.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'UserId' => 'required',
+            'Email' => 'required|email|unique:contact,email',
+            'Phonenumber' => 'required',
+            'IsActive' => 'required',
+            'Note' => 'nullable',
+        ]);
+
+        Contact::create([
+            'UserId' => 1,
+            'Email' => $request->Email,
+            'Phonenumber' => $request->Phonenumber,
+            'IsActive' => 1,
+            'Note' => $request->Note,
+        ]);
+
+        return redirect()->route('contact.index')
+            ->with('success', 'Contact created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Contact $contact)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+
+        return redirect()->route('contact.index')
+            ->with('success', 'Contact deleted successfully');
+    }
+}
---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Overview</title>
+</head>
+
+<body>
+    <nav></nav>
+    <div class="container">
+        <table>
+            <thead>
+                <tr>
+                    <th>UserId</th>
+                    <th>Email</th>
+                    <th>Phonenumber</th>
+                    <th>Note</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($contact as $contact)
+                    <tr>
+                        <td>{{ $contact->UserId }}</td>
+                        <td>{{ $contact->Email }}</td>
+                        <td>{{ $contact->Phonenumber }}</td>
+                        <td>{{ $contact->Note }}</td>
+                        <td>
+                            <form method="post" action="{{ route('contact.destroy', $contact->id) }}">
+                                @csrf
+                                @method('DELETE')
+                                <input type="submit" value="Delete">
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{-- link to add new contact --}}
+        <a href="{{ route('contact.create') }}">Add new contact</a>
+    </div>
+</body>
+
+</html>
