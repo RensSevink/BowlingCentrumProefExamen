32285ef3fbca4c21488ba31dd8dccf3b70ab97f8
RensSevink <332726@student.mboutrecht.nl>
2023-03-28 10:25:28 +0200

    migrations fixed
    int veranderd naar interger

--- database/migrations/2023_03_28_072013_create_contacts_table.php
+++ database/migrations/2023_03_28_072013_create_contacts_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             // string
             $table->string('Email');
             // int
-            $table->int('Phonenumber');
+            $table->integer('Phonenumber');
             // boolean
             $table->boolean('IsActive');
             // string
